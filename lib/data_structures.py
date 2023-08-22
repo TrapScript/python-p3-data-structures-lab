06972c1666f371386b173a7797d7e77709dde54c
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-08-22 17:35:13 +0300

    done

--- lib/data_structures.py
+++ lib/data_structures.py
@@ -17,22 +17,51 @@ spicy_foods = [
 ]
 
 def get_names(spicy_foods):
-    pass
+    names = []
+    for food in spicy_foods:
+        names.append(food['name'])
+    return names
+
+
+    
 
 def get_spiciest_foods(spicy_foods):
-    pass
+    sfoods = []
+    for sfood in spicy_foods:
+        if sfood['heat'] > 5:
+            sfoods.append(food)
+        return sfoods
 
 def print_spicy_foods(spicy_foods):
-    pass
+    for fud in spicy_foods:
+        heat_level = "🌶" * food['heat_level']
+        print(f"{fud['name']} ({fud['cuisine']}) | Heat Level: {heat_level}")
 
 def get_spicy_food_by_cuisine(spicy_foods, cuisine):
-    pass
+    for food in spicy_foods:
+        if food['cuisine'] == cuisine:
+            return food
+        return None
 
 def print_spiciest_foods(spicy_foods):
-    pass
+    spiciest_food = []
+    for food in spicy_foods:
+        if food['heat_level'] >5:
+            spiciest_food.append(food)
+
+    def print_spicy_foods(spicy_foods):
+        heat_level = "🌶" * food['heat_level']
+        print(f"{food['name']} ({food['cuisine']}) | Heat Level: {heat_level}")
+    
 
 def get_average_heat_level(spicy_foods):
-    pass
+    total_heat = sum(food['heat_level'] for food in spicy_foods)
+    num_foods = len(spicy_foods)
+    if num_food == 0:
+        return 0
+    average = total_heat / num_foods
+    return average
+
 
 def create_spicy_food(spicy_foods, spicy_food):
     pass
